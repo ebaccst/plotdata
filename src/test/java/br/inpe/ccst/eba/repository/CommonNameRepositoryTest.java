71726011452656308d72067093ff5fa50cf8c5bb
Heitor Guerra Carneiro <heitorguerrac@gmail.com>
2017-10-18 11:04:45 -0200

    #1. Updating Repositories

--- src/test/java/br/inpe/ccst/eba/repository/CommonNameRepositoryTest.java
+++ src/test/java/br/inpe/ccst/eba/repository/CommonNameRepositoryTest.java
@@ -4,18 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CommonNameRepositoryTest {
-	
+import br.inpe.ccst.eba.AbstractTest;
+
+public class CommonNameRepositoryTest extends AbstractTest {
+
 	@Autowired
 	private CommonNameRepository repository;
-	
+
 	public void shouldRepositoryNotBeNull() {
 		assertNotNull(this.repository);
 	}
--- src/test/java/br/inpe/ccst/eba/repository/TaxonomyRepositoryTest.java
+++ src/test/java/br/inpe/ccst/eba/repository/TaxonomyRepositoryTest.java
@@ -4,24 +4,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class TaxonomyRepositoryTest {
-	
+import br.inpe.ccst.eba.AbstractTest;
+
+public class TaxonomyRepositoryTest extends AbstractTest {
+
 	@Autowired
 	private TaxonomyRepository repository;
-
 
 	@Test
 	public void shouldRepositoryNotBeNull() {
 		assertNotNull(this.repository);
 	}
-	
+
 	@Test
 	public void shouldGetCountOfRows() throws Exception {
 		assertEquals(new Integer(16467), this.repository.getCountOfRecords());
